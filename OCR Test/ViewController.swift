48198172ac688694a2ba86d7fe84c03348c5e58f
chagmn <sopg4@naver.com>
2020-09-03 15:56:30 +0900

    Initial Commit

---
+++ OCR Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR Test
+//
+//  Created by 창민 on 2020/09/03.
+//  Copyright © 2020 창민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
